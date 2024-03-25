453651623eb100f4e9754377c8c2dd25152fc1e9
Zamarr0n <emilio.zamarrons@gmail.com>
2024-03-24 18:40:38 -0600

    .

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -8,9 +8,7 @@ class Program
 
 // DateTime startTime = DateTime.Now;
 // DateTime futureTime = startTime.AddSeconds(5);
-
 // Thread.Sleep(3000);
-
 // DateTime currentTime = DateTime.Now;
 // if (currentTime < futureTime)
 // {
